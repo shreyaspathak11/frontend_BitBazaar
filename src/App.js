326f126fdb97f3481f11660067c5eae8bd10c2b6
Shreyas Pathak <97246657+shreyaspathak11@users.noreply.github.com>
2023-05-11 23:56:23 +0530

    error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Coins from "./components/Coins";
 import CoinDetails from "./components/CoinDetails";
 import Exchanges from "./components/Exchanges";
@@ -9,26 +7,24 @@ import Register from "./components/Register";
 import Terms from "./components/Terms";
 import Newsletter from "./components/Newsletter";
 import Login from "./components/LogIn";
-import WelcomePage from "./components/WelcomePage";
+
 
 function App() {
   return (
+    <>    
     <Router>
-      <Header />
       <Routes>
-        <Route path="/welcome" element={<WelcomePage />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/coins" element={<Coins />} />
         <Route path="/coins/:coinId" element={<CoinDetails />} />
         <Route path="/exchanges" element={<Exchanges />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/*" element={<Login />} />
         <Route path="/terms" element={<Terms />} />
         <Route path="/newsletter" element={<Newsletter />} />
       </Routes>
-      <Footer />
     </Router>
-    
+    </>
   );
 }
 
